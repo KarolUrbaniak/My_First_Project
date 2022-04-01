8bccb5bea656f365ab18abd69e7f4ed73a786c99
Karol <101590093+KarolUrbaniak@users.noreply.github.com>
2022-04-01 02:44:33 +0200

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,19 +1,5 @@
-// Alright so we've discussed the way you can grab html elements, let's
-// see how we can interact with them in Javascript!
-
-// Type this into your console:
-
 // Grab the Header with h1
 var header = document.getElementById("about")
-
-// Then you can interface with the object.
-
-// Interface with the style.
-//You will see a ton of options show up!
-
-// Now let's connect it to the script to
-// change it once every second to a random color!
-
 // Random Color Function:
 
 // http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
